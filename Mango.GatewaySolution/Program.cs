315f676966b1e707791e5c2a00bb24f18766f416
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-03-12 23:36:59 +1100

    configure API and web service

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -4,7 +4,12 @@ using Ocelot.Middleware;
 using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.AddAppAuthetication();
+
+
+
+
 if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
 {
     builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
@@ -13,11 +18,12 @@ else
 {
     builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 }
+
 builder.Services.AddOcelot(builder.Configuration);
 
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
 app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
--- Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -91,6 +91,7 @@ namespace Mango.Services.OrderAPI.Controllers
                 orderHeaderDto.Status = SD.Status_Pending;
                 orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
                 orderHeaderDto.OrderTotal = Math.Round(orderHeaderDto.OrderTotal, 2);
+                
                 OrderHeader orderCreated = _db.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
                 await _db.SaveChangesAsync();
 
